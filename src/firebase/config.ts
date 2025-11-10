1d2be1e0f45cc3d328bd0dd45c6c353a15074af9
Vignesh Narayanan <vignesh@zamp.ai>
2025-11-10 20:17:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-921060813-5a0d7",
+  "appId": "1:287688033905:web:c8ede54b2e1e535a9cbdd0",
+  "apiKey": "AIzaSyAafJEuBmMK0Ic0XxHKsXAkj1waf1LSdpY",
+  "authDomain": "studio-921060813-5a0d7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "287688033905"
+};
